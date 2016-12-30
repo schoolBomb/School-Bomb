c0a20c1ecc1a410da26d9aefef2219f429ea21a2
Dongsarang <jde0913@gmail.com>
2016-12-30 11:07:06 +0900

    16.12.30
    prefab folder and other data

---
+++ School Bomb/Assets/Scripts/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;//for serializable
+using System.Collections;//for list and other data structure
+
+//객체가 필요없는 클래스고 함수만 사용하면 되는 클래스이기 때문에 static class로 선언한다. 
+//따라서 모든 함수도 static.
+public static class JsonHelper{
+	//사용할 구조체나 클래스가 여러 종류라는 가정하에 일반화 사용.
+	public static T[] FromJson<T>(string json){//json->object
+		Wrapper<T> wrapper = UnityEngine.JsonUtility.FromJson<Wrapper<T>> (json);
+		return wrapper.Items;
+	}
+
+	public static string ToJson<T>(T[] array){//object->json
+		Wrapper<T> wrapper=new Wrapper<T>();
+		wrapper.Items=array;
+		return UnityEngine.JsonUtility.ToJson (wrapper);
+	}
+
+	[Serializable]//직렬화
+	private class Wrapper<T>{//wrapper는 이 클래스에서 밖에 사용을 안 하므로 priate 클래스로 만들었다.
+		public T[] Items;
+	}
+}
---
+++ School Bomb/Assets/Scripts/JsonReader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;//for textAsset
+using System.Collections;
+using System;
+
+public class JsonReader : MonoBehaviour {
+	Word[] w;
+
+	// Use this for initialization
+	void Start () {
+		//txt(json) 파일을 읽어들인다.
+		TextAsset t=(TextAsset)AssetDatabase.LoadAssetAtPath("Assets/Resource/wordSheet.txt", typeof(TextAsset));
+		if (t == null) {//파일 로드가 안 된 경우
+			Debug.Log ("wordSheet can't be loaded");
+		}
+
+		//배열인 json 데이터 구조를 읽어들인다.
+		string tempData = "{\"Items\":" + t.text + "}";
+		w = JsonHelper.FromJson<Word> (tempData);
+		if (w == null)// if it cannot change string to object
+			Debug.Log ("Can't make object");
+		Debug.Log (w[0].isQuestion);
+	}
+}
